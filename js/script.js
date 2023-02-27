e24d763c7821e748732af2aa46c5f0e99ec24c2b
DaniloDuarte <danilojoseduarte@gmail.com>
2023-02-27 14:29:23 -0300

    Aplicando JavaSript nav-menu

--- js/script.js
+++ js/script.js
@@ -9,3 +9,11 @@ darkmode.onclick = () => {
         document.body.classList.remove('color');
     }
 }
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
