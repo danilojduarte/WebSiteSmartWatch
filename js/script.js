b94ce4984b699105e757d504aac183010a665f96
DaniloDuarte <danilojoseduarte@gmail.com>
2023-02-27 14:36:43 -0300

    Projeto finalizado!

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ darkmode.onclick = () => {
         darkmode.classList.replace('bx-sun', 'bx-moon');
         document.body.classList.remove('color');
     }
-}
+};
 
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
@@ -16,4 +16,19 @@ let navlist = document.querySelector('.navlist');
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('open');
-}
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+};
+
+const sr = ScrollReveal ({
+    distance: '70px',
+    duration: 2700,
+    reset: true
+});
+
+sr.reveal('.hero-text', {delay:200, origin: 'bottom'});
+sr.reveal('.hero-img', {delay:350, origin: 'top'});
+sr.reveal('.down-arrow', {delay:450, origin: 'right'});
